313064f389826c760e203fd5c361a48f9a880e49
Ross Wintle <rosswintle@users.noreply.github.com>
2023-02-22 11:25:48 +0000

    Prevent back-end data/query caching on front-page

--- wp-templates/front-page.js
+++ wp-templates/front-page.js
@@ -23,6 +23,7 @@ const postsPerPage = 3;
 export default function Component() {
   const { data, loading } = useQuery(Component.query, {
     variables: Component.variables(),
+    fetchPolicy: 'no-cache',
   });
   if (loading) {
     return null;
