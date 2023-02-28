92b59d35e2bb32c49c80d4700edf77da3d1cf633
Ross Wintle <rosswintle@users.noreply.github.com>
2023-02-28 13:03:00 +0000

    Revert "Modify front page cache policy to something better."
    This reverts commit a33ae6648139cfba37bc79642a879a762fb1418d.

--- wp-templates/front-page.js
+++ wp-templates/front-page.js
@@ -23,7 +23,7 @@ const postsPerPage = 3;
 export default function Component() {
   const { data, loading } = useQuery(Component.query, {
     variables: Component.variables(),
-    fetchPolicy: 'cache-and-network',
+    fetchPolicy: 'no-cache',
   });
   if (loading) {
     return null;
